141059b7ccdc1484baa1abcbc0fcbdef5dc17051
Faraz-Sarwar <farazsarwar77@gmail.com>
2026-09-12 16:25:37 +0500

    Fixed username bug

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rentee_real_estate/Utilities/colors.dart';
+import 'package:rentee_real_estate/models/user_model.dart';
 import 'package:rentee_real_estate/view_models/auth_vm/auth_vm.dart';
 import 'package:rentee_real_estate/view_models/data_vm/user_data.dart';
 
@@ -9,9 +10,14 @@ final propertyProvider = FutureProvider((ref) async {
   return await ref.read(dataProviderVm).loadProperties();
 });
 
-final userInfoProvider = FutureProvider((ref) async {
-  final uid = FirebaseAuth.instance.currentUser!.uid;
-  return await ref.read(dataProviderVm).loadUserInfo(uid);
+final authStateProvider = StreamProvider<User?>((ref) {
+  return FirebaseAuth.instance.authStateChanges();
+});
+
+final userInfoProvider = FutureProvider.autoDispose((ref) async {
+  final user = ref.watch(authStateProvider).value;
+  if (user == null) return null;
+  return ref.read(dataProviderVm).loadUserInfo(user.uid);
 });
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -31,7 +37,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         title: userInfo.when(
           data: (data) {
             return Text(
-              "Welcome ${data.name}",
+              "Welcome ${data!.name}",
               style: TextStyle(color: AppColors.textPrimary),
             );
           },
