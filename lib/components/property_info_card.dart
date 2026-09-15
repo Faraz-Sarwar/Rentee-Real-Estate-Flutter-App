f4c1666070374d671c3b54769b93c65e6b1c2a47
Faraz-Sarwar <farazsarwar77@gmail.com>
2026-09-15 17:10:13 +0500

    All set for home and filtering

---
+++ lib/components/property_info_card.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:rentee_real_estate/Utilities/app_colors.dart';
+import 'package:rentee_real_estate/Utilities/app_sizing.dart';
+import 'package:rentee_real_estate/components/property_details_chip.dart';
+import 'package:rentee_real_estate/models/property_model.dart';
+
+class PropertyInfoCard extends StatelessWidget {
+  final PropertyModel property;
+  const PropertyInfoCard({super.key, required this.property});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.35,
+      margin: EdgeInsets.symmetric(vertical: AppSize.medium),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              property.imageUrl,
+              height: 200,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              errorBuilder: (context, error, stackTrace) => Container(
+                height: 200,
+                color: Colors.grey[300],
+                child: const Icon(Icons.broken_image),
+              ),
+            ),
+          ),
+          const SizedBox(height: AppSize.small),
+          Padding(
+            padding: const EdgeInsets.all(AppSize.medium),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      property.name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$${property.price.toString()}/m',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: AppSize.small),
+                // Container(
+                //   decoration: BoxDecoration(
+                //     color: AppColors.background,
+                //     borderRadius:
+                //         BorderRadius.circular(12),
+                //   ),
+                //   // (.split) breaks the string into List
+                //   // based on some pattern defined
+                //   // "Hello, Hi" becomes [Hello, Hi]
+                //   // I used .split to // Get (city) location from
+                //   // the full location ({city + State})
+                //   // Eg (Denver, Colorado,) => Denver
+                //   // Eg (Seattle Washington) => Seattle
+                //   child: Center(
+                //     child: Text(
+                //       property.location
+                //           .split(',')[0]
+                //           .toString(),
+                //     ),
+                //   ),
+                // ),
+                Row(
+                  children: [
+                    PropertyDetailsChip(
+                      property: property,
+                      icon: Icons.location_on_outlined,
+                      text: property.location.split(',')[0].toString(),
+                    ),
+                    const SizedBox(width: AppSize.small),
+                    PropertyDetailsChip(
+                      property: property,
+                      icon: Icons.star_border,
+                      text: property.rating.toString(),
+                    ),
+                    const SizedBox(width: AppSize.small),
+                    PropertyDetailsChip(
+                      property: property,
+                      icon: Icons.bed_rounded,
+                      text: '${property.bedrooms.toString()} Bed',
+                    ),
+                    const SizedBox(width: AppSize.small),
+                    PropertyDetailsChip(
+                      property: property,
+                      icon: Icons.shower,
+                      text: '${property.bedrooms.toString()} Bath',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:rentee_real_estate/Utilities/app_colors.dart';
 import 'package:rentee_real_estate/Utilities/app_sizing.dart';
 import 'package:rentee_real_estate/components/custom_text_field.dart';
 import 'package:rentee_real_estate/components/property_details_chip.dart';
+import 'package:rentee_real_estate/components/property_info_card.dart';
 import 'package:rentee_real_estate/models/property_model.dart';
 import 'package:rentee_real_estate/view_models/auth_vm/auth_vm.dart';
 import 'package:rentee_real_estate/view_models/data_vm/user_data.dart';
@@ -225,133 +226,134 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                             itemBuilder: (context, index) {
                               final property = filteredProperties[index];
 
-                              return Container(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.35,
-                                margin: EdgeInsets.symmetric(
-                                  vertical: AppSize.medium,
-                                ),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: AppColors.white,
-                                  borderRadius: BorderRadius.circular(24),
-                                ),
-                                child: Column(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(20),
-                                      child: Image.network(
-                                        property.imageUrl,
-                                        height: 200,
-                                        fit: BoxFit.cover,
-                                        width: double.infinity,
-                                        errorBuilder:
-                                            (context, error, stackTrace) =>
-                                                Container(
-                                                  height: 200,
-                                                  color: Colors.grey[300],
-                                                  child: const Icon(
-                                                    Icons.broken_image,
-                                                  ),
-                                                ),
-                                      ),
-                                    ),
-                                    const SizedBox(height: AppSize.small),
-                                    Padding(
-                                      padding: const EdgeInsets.all(
-                                        AppSize.medium,
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(
-                                                property.name,
-                                                style: TextStyle(
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                              Text(
-                                                '\$${property.price.toString()}/m',
-                                                style: TextStyle(
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          const SizedBox(height: AppSize.small),
-                                          // Container(
-                                          //   decoration: BoxDecoration(
-                                          //     color: AppColors.background,
-                                          //     borderRadius:
-                                          //         BorderRadius.circular(12),
-                                          //   ),
-                                          //   // (.split) breaks the string into List
-                                          //   // based on some pattern defined
-                                          //   // "Hello, Hi" becomes [Hello, Hi]
-                                          //   // I used .split to // Get (city) location from
-                                          //   // the full location ({city + State})
-                                          //   // Eg (Denver, Colorado,) => Denver
-                                          //   // Eg (Seattle Washington) => Seattle
-                                          //   child: Center(
-                                          //     child: Text(
-                                          //       property.location
-                                          //           .split(',')[0]
-                                          //           .toString(),
-                                          //     ),
-                                          //   ),
-                                          // ),
-                                          Row(
-                                            children: [
-                                              PropertyDetailsChip(
-                                                property: property,
-                                                icon:
-                                                    Icons.location_on_outlined,
-                                                text: property.location
-                                                    .split(',')[0]
-                                                    .toString(),
-                                              ),
-                                              const SizedBox(
-                                                width: AppSize.small,
-                                              ),
-                                              PropertyDetailsChip(
-                                                property: property,
-                                                icon: Icons.star_border,
-                                                text: property.rating
-                                                    .toString(),
-                                              ),
-                                              const SizedBox(
-                                                width: AppSize.small,
-                                              ),
-                                              PropertyDetailsChip(
-                                                property: property,
-                                                icon: Icons.bed_rounded,
-                                                text:
-                                                    '${property.bedrooms.toString()} Bed',
-                                              ),
-                                              const SizedBox(
-                                                width: AppSize.small,
-                                              ),
-                                              PropertyDetailsChip(
-                                                property: property,
-                                                icon: Icons.shower,
-                                                text:
-                                                    '${property.bedrooms.toString()} Bath',
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
+                              return PropertyInfoCard(property: property);
+                              // return Container(
+                              //   height:
+                              //       MediaQuery.of(context).size.height * 0.35,
+                              //   margin: EdgeInsets.symmetric(
+                              //     vertical: AppSize.medium,
+                              //   ),
+                              //   width: double.infinity,
+                              //   decoration: BoxDecoration(
+                              //     color: AppColors.white,
+                              //     borderRadius: BorderRadius.circular(24),
+                              //   ),
+                              //   child: Column(
+                              //     children: [
+                              //       ClipRRect(
+                              //         borderRadius: BorderRadius.circular(20),
+                              //         child: Image.network(
+                              //           property.imageUrl,
+                              //           height: 200,
+                              //           fit: BoxFit.cover,
+                              //           width: double.infinity,
+                              //           errorBuilder:
+                              //               (context, error, stackTrace) =>
+                              //                   Container(
+                              //                     height: 200,
+                              //                     color: Colors.grey[300],
+                              //                     child: const Icon(
+                              //                       Icons.broken_image,
+                              //                     ),
+                              //                   ),
+                              //         ),
+                              //       ),
+                              //       const SizedBox(height: AppSize.small),
+                              //       Padding(
+                              //         padding: const EdgeInsets.all(
+                              //           AppSize.medium,
+                              //         ),
+                              //         child: Column(
+                              //           crossAxisAlignment:
+                              //               CrossAxisAlignment.start,
+                              //           children: [
+                              //             Row(
+                              //               mainAxisAlignment:
+                              //                   MainAxisAlignment.spaceBetween,
+                              //               children: [
+                              //                 Text(
+                              //                   property.name,
+                              //                   style: TextStyle(
+                              //                     fontSize: 20,
+                              //                     fontWeight: FontWeight.bold,
+                              //                   ),
+                              //                 ),
+                              //                 Text(
+                              //                   '\$${property.price.toString()}/m',
+                              //                   style: TextStyle(
+                              //                     fontSize: 18,
+                              //                     fontWeight: FontWeight.bold,
+                              //                   ),
+                              //                 ),
+                              //               ],
+                              //             ),
+                              //             const SizedBox(height: AppSize.small),
+                              //             // Container(
+                              //             //   decoration: BoxDecoration(
+                              //             //     color: AppColors.background,
+                              //             //     borderRadius:
+                              //             //         BorderRadius.circular(12),
+                              //             //   ),
+                              //             //   // (.split) breaks the string into List
+                              //             //   // based on some pattern defined
+                              //             //   // "Hello, Hi" becomes [Hello, Hi]
+                              //             //   // I used .split to // Get (city) location from
+                              //             //   // the full location ({city + State})
+                              //             //   // Eg (Denver, Colorado,) => Denver
+                              //             //   // Eg (Seattle Washington) => Seattle
+                              //             //   child: Center(
+                              //             //     child: Text(
+                              //             //       property.location
+                              //             //           .split(',')[0]
+                              //             //           .toString(),
+                              //             //     ),
+                              //             //   ),
+                              //             // ),
+                              //             Row(
+                              //               children: [
+                              //                 PropertyDetailsChip(
+                              //                   property: property,
+                              //                   icon:
+                              //                       Icons.location_on_outlined,
+                              //                   text: property.location
+                              //                       .split(',')[0]
+                              //                       .toString(),
+                              //                 ),
+                              //                 const SizedBox(
+                              //                   width: AppSize.small,
+                              //                 ),
+                              //                 PropertyDetailsChip(
+                              //                   property: property,
+                              //                   icon: Icons.star_border,
+                              //                   text: property.rating
+                              //                       .toString(),
+                              //                 ),
+                              //                 const SizedBox(
+                              //                   width: AppSize.small,
+                              //                 ),
+                              //                 PropertyDetailsChip(
+                              //                   property: property,
+                              //                   icon: Icons.bed_rounded,
+                              //                   text:
+                              //                       '${property.bedrooms.toString()} Bed',
+                              //                 ),
+                              //                 const SizedBox(
+                              //                   width: AppSize.small,
+                              //                 ),
+                              //                 PropertyDetailsChip(
+                              //                   property: property,
+                              //                   icon: Icons.shower,
+                              //                   text:
+                              //                       '${property.bedrooms.toString()} Bath',
+                              //                 ),
+                              //               ],
+                              //             ),
+                              //           ],
+                              //         ),
+                              //       ),
+                              //     ],
+                              //   ),
+                              // );
                             },
                           ),
                         );
@@ -377,10 +379,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                             itemBuilder: (context, index) {
                               final PropertyModel property =
                                   filteredProperties[index];
-                              return ListTile(
-                                contentPadding: EdgeInsets.zero,
-                                title: Text(property.name),
-                              );
+                              return PropertyInfoCard(property: property);
                             },
                           ),
                         );
